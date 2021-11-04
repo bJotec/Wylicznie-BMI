d783baf2aa11ddbef987e2bfc8068f0449ff27d5
Bartlomiej Jarzebak <bjotec@gmail.com>
2021-11-04 20:45:00 +0100

    Dodanie przypadku braku id

--- src/pl/camp/it/App.java
+++ src/pl/camp/it/App.java
@@ -23,6 +23,7 @@ public class App {
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             String choose = reader.readLine();
+            int id = random.nextInt(101);
 
             switch (choose) {
                 case "1":
@@ -33,8 +34,6 @@ public class App {
 
                     Double cal = BMI.calculate(weight, height);
 
-                    int id = random.nextInt(101);
-
                     IDBMI.put(id, new BMI(weight, height, cal));
 
                     System.out.println(" Twoje unikalne ID pod którym znajduje się BMI to : " + id);
@@ -43,15 +42,18 @@ public class App {
                 case "2":
                     System.out.println("Podaj ID żeby dowiedziec się jakie masz BMI: ");
                     Integer writeID = Integer.valueOf(reader.readLine());
-// ********************** próba na ifie niedziałająca
-                    if (writeID != IDBMI.get(writeID).keySet()) {
-                        System.out.println("nie ma takiego id");
-                    } else {
-                        System.out.println("Twoje BMI to " + IDBMI.get(writeID).getCal());
-                        System.out.println("dla wagi ciała : " + IDBMI.get(writeID).getWeight() + "kg" + " oraz wzrostu : " + IDBMI.get(writeID).getHeight() + "cm");
-                        System.out.println("oraz wzrostu : " + IDBMI.get(writeID).getHeight() + "cm");
-                    }
 
+                    for ( Integer key : IDBMI.keySet() ) {
+
+                        if (writeID != key) {
+                            System.out.println("nie ma takiego id");
+                            break;
+                        } else {
+                            System.out.println("Twoje BMI to " + IDBMI.get(writeID).getCal());
+                            System.out.println("dla wagi ciała : " + IDBMI.get(writeID).getWeight() + "kg" + " oraz wzrostu : " + IDBMI.get(writeID).getHeight() + "cm");
+                            System.out.println("oraz wzrostu : " + IDBMI.get(writeID).getHeight() + "cm");
+                        }
+                    }
                     break;
 
                 case "3":
